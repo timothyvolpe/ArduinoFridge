9a96073bd2c8dac5fc2923baa4f9b59cf087dd66
tvolpes <timothybvolpe@gmail.com>
2015-11-25 22:22:56 -0500

    Added error pin; reads force from analog

--- ArduinoFridge/ArduinoFridge.ino
+++ ArduinoFridge/ArduinoFridge.ino
@@ -4,12 +4,50 @@
  Author:	Timothy
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#include "def.h"
+#include "fridge.h"
 
+bool isErrorLit, isError;
+CFridge *pFridge;
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+	isError = false;
+	isErrorLit = false;
+	pFridge = NULL;
+
+	// Setup digital pins
+	pinMode( DIGITAL_PIN_FORCE, OUTPUT ); // Force LED
+	pinMode( DIGITAL_PIN_ERROR, OUTPUT ); // Error LED
+
+	// Create the fridge class
+	pFridge = new CFridge();
+	if( !pFridge )
+		isError = true;
+
+	// Serial
+	Serial.begin( 9600 );
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+	int iForceVal;
+
+	// Check for error
+	if ( isError && !isErrorLit ) {
+		isErrorLit = true;
+		digitalWrite( DIGITAL_PIN_ERROR, HIGH ); // Turn on the error led
+	}
+
+	// Check for force
+	iForceVal = analogRead( ANALOG_PIN_FORCE );
+	if( iForceVal > 512 )
+		digitalWrite( DIGITAL_PIN_FORCE, HIGH );
+	else
+		digitalWrite( DIGITAL_PIN_FORCE, LOW );
+
+	// Print a test value
+	Serial.println( iForceVal );
 }
